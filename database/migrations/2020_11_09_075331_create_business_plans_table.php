31724424b533c0147183a04d09862a3f82e7e215
dang_khoa123 <dang_khoa@lampart-vn.com>
2020-11-09 14:57:30 +0700

    add migrate

---
+++ database/migrations/2020_11_09_075331_create_business_plans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_plan', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('maximum_storage_file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_plan');
+    }
+}
