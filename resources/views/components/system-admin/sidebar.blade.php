ddcfbcd83bd3173c88d88011be989ef2e5c87ac0
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2020-11-11 09:23:16 +0700

    add sidebar

--- resources/views/components/system-admin/sidebar.blade.php
+++ resources/views/components/system-admin/sidebar.blade.php
@@ -1,1 +1,5 @@
-This is the master sidebar of system admin
+<div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Business plan</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Company</a>
+</div>
