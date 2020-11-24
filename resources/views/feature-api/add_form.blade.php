a6ba13552d6ddd1ffaa25d4a1d034bf958af384a
dang_khoa123 <dang_khoa@lampart-vn.com>
2020-11-24 14:36:12 +0700

    update cancel button

--- resources/views/feature-api/add_form.blade.php
+++ resources/views/feature-api/add_form.blade.php
@@ -31,7 +31,7 @@
                     @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
-                <a class="btn btn-danger ml-2" href="{{ route('role.list') }}" role="button">Cancel</a>
+                <a class="btn btn-danger ml-2" href="{{ route('feature-api.list') }}" role="button">Cancel</a>
             </form>
         </div>
     </div>
