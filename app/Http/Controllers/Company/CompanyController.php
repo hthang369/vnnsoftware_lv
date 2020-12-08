ef22bf45c79626678bef255aebb97beee23d09c5
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2020-12-08 10:52:09 +0700

    fix conflic

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -13,13 +13,11 @@ class CompanyController extends Controller
 
     private $companyService;
     private $businessPlanService;
-    private $companyValidation;
 
-    public function __construct(CompanyService $companyService, BusinessPlanService $businessPlanService, CompanyValidation $companyValidation)
+    public function __construct(CompanyService $companyService, BusinessPlanService $businessPlanService)
     {
         $this->companyService = $companyService;
         $this->businessPlanService = $businessPlanService;
-        $this->companyValidation = $companyValidation;
     }
 
     public function index()
@@ -42,54 +40,12 @@ class CompanyController extends Controller
 
     public function register(Request $request)
     {
-<<<<<<< HEAD
-
-        $validator = $this->companyValidation->newValidate($request->all());
-
-        if ($validator->fails()) {
-            return redirect()->back()->withInput()->withErrors($validator->errors());
-        }
-
-        $input = $request->all();
-
-        try {
-            $company = $this->companyService->create($input);
-            return redirect()->intended('/system-admin/company/detail/' . $company->id);
-        } catch (\Exception $ex) {
-            abort(500);
-        }
-=======
         return $this->companyService->Create($request);
->>>>>>> origin/master
     }
 
     public function update($id, Request $request)
     {
-<<<<<<< HEAD
-        $company = $this->companyService->getById($id);
-
-        if (is_null($company)) {
-            abort(404,'Page not found');
-        }
-
-        $validator = $this->companyValidation->updateValidate($request->all(), $id);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withInput()->withErrors($validator->errors());
-        }
-
-        $input = request()->except(['_token', 'role']);
-
-        try {
-            $company = $this->companyService->update($id, $input);
-        } catch (\Exception $ex) {
-            abort(500);
-        }
-
-        return redirect()->intended('/system-admin/company/detail/' . $id)->with('saved', true);
-=======
         return $this->companyService->update($id, $request);
->>>>>>> origin/master
     }
 
     public function delete($id)
