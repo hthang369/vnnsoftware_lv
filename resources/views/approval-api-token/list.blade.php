d72b36000efe154a12e3ea58f1854937ad510ede
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2020-12-14 16:01:52 +0700

    api token

--- resources/views/approval-api-token/list.blade.php
+++ resources/views/approval-api-token/list.blade.php
@@ -20,7 +20,12 @@
             <strong>Saved!</strong>
         </div>
     @endif
-    @if(count($json) == 0)
+    @if(session()->has('error'))
+        <div class="alert alert-success">
+            <strong>System error!</strong>
+        </div>
+    @endif
+    @if(count($data) == 0)
         <div class="alert alert-warning">
             <strong>Sorry!</strong> No Item Found.
         </div>
@@ -35,13 +40,10 @@
                     <th scope="col">@lang('custom_label.request_approval_status')</th>
                     <th scope="col">@lang('custom_label.request_approval_timestamp')</th>
                     <th scope="col">@lang('custom_label.action')</th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($json as $i => $item)
+                @foreach($data as $i => $item)
                     <tr>
                         <td>{{$i + 1}}</td>
                         <td>{{$item['id']}}</td>
@@ -52,18 +54,13 @@
                             @if($status[$item['request_approval_status']] == 'Not accepted')
                                 <a class="btn btn-info m-1" href="/system-admin/approval-api-token/approval-token/{{$item['id']}}" role="button">Accept</a>
                             @endif
-                        </td>
-                        <td>
                             @if($status[$item['request_approval_status']] == 'Accepted')
                                 <a class="btn btn-primary m-1" href="/system-admin/approval-api-token/stop-token/{{$item['id']}}" role="button">Pause</a>
                             @endif
-                        </td>
-                        <td>
                             @if($status[$item['request_approval_status']] == 'Paused')
-                                <a class="btn btn-primary m-1" href="/system-admin/approval-api-token/re-open/{{$item['id']}}" role="button">Reopen</a>
+                                <a class="btn btn-info m-1" href="/system-admin/approval-api-token/approval-token/{{$item['id']}}" role="button">Accept</a>
+                                {{--<a class="btn btn-primary m-1" href="/system-admin/approval-api-token/reopen-token/{{$item['id']}}" role="button">Reopen</a>--}}
                             @endif
-                        </td>
-                        <td>
                             <a class="btn btn-danger m-1" onclick="return confirm('@lang('custom_message.confirm_delete')');" href="/system-admin/approval-api-token/delete-token/{{$item['id']}}" role="button">Delete</a>
                         </td>
                     </tr>
