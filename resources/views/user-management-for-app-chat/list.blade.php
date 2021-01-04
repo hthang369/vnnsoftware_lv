f95d4c31a1862aef6f5969a3b72fb96fe59e8ceb
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2021-01-04 11:58:43 +0700

    user management for app chat list

--- resources/views/user-management-for-app-chat/list.blade.php
+++ resources/views/user-management-for-app-chat/list.blade.php
@@ -8,15 +8,15 @@
 
 @section('content')
     <div class="alert alert-primary" role="alert">
-        <h1>@lang('custom_title.company')</h1>
+        <h1>@lang('custom_title.user_management_for_app_chat')</h1>
     </div>
-    <a class="my-2 btn btn-primary" href="/system-admin/company/new" role="button">+ @lang('custom_label.add_new')</a>
+    <a class="my-2 btn btn-primary" href="/system-admin/user-management-for-app-chat/new" role="button">+ @lang('custom_label.add_new')</a>
     @if(session()->has('deleted'))
         <div class="alert alert-success">
             <strong>@lang('custom_message.deleted')</strong>
         </div>
     @endif
-    @if(count($list) == 0)
+    @if(0 == 0)
         <div class="alert alert-warning">
             @lang('custom_message.no_item_found')
         </div>
