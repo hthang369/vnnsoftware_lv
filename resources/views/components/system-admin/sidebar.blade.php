2e55fe6ab922abaa122ffb0b2c739a76743d6b5d
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2021-01-21 10:23:39 +0700

    permission...

--- resources/views/components/system-admin/sidebar.blade.php
+++ resources/views/components/system-admin/sidebar.blade.php
@@ -2,10 +2,9 @@
     <div class="list-group list-group-flush">
         @foreach($LEFTMENU as $item)
             @if(substr(Route::currentRouteName(), 0, strpos(Route::currentRouteName(), '.')) == $item->prefix)
-                <a href="{{$item->url}}"
-                   class="list-group-item list-group-item-action
-               {{ Route::currentRouteName() == $item->route_name ? 'active font-weight-bold bg-info' : '' }}">@lang($item->lang)
-                </a>
+                @if(!in_array($item->route_name, $NOT_HAS_PERMISSION))
+                    <a href="{{$item->url}}" class="list-group-item list-group-item-action {{ Route::currentRouteName() == $item->route_name ? ' active font-weight-bold bg-info' : '' }}">@lang($item->lang)</a>
+                @endif
             @endif
         @endforeach
     </div>
--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ Route::group(['prefix' => 'system-admin', 'middleware' => ['auth', 'permission']
     Route::group(['prefix' => 'company'], function () {
         Route::get('/', 'Company\CompanyController@index')->name('LAKA company manage.Company list');
         Route::get('detail/{id}', 'Company\CompanyController@detail')->name('LAKA company manage.Detail');
-        Route::get('update/{id}', 'Company\CompanyController@updateForm')->name('LAKA company manage.Update company info.form');
+        Route::get('update/{id}', 'Company\CompanyController@updateForm')->name('LAKA company manage.Update company info');
         Route::post('update/{id}', 'Company\CompanyController@update')->name('LAKA company manage.Update company info');
-        Route::get('new', 'Company\CompanyController@newForm')->name('LAKA company manage.Add company.form');
+        Route::get('new', 'Company\CompanyController@newForm')->name('LAKA company manage.Add company');
         Route::post('new', 'Company\CompanyController@register')->name('LAKA company manage.Add company');
         Route::get('delete/{id}', 'Company\CompanyController@delete')->name('LAKA company manage.Delete company');
     });
@@ -41,9 +41,9 @@ Route::group(['prefix' => 'system-admin', 'middleware' => ['auth', 'permission']
     Route::group(['prefix' => 'business-plan'], function () {
         Route::get('/', 'BusinessPlan\BusinessPlanController@index')->name('LAKA business plan.Business plan list');
         Route::get('detail/{id}', 'BusinessPlan\BusinessPlanController@detailForm')->name('LAKA business plan.Detail');
-        Route::get('new', 'BusinessPlan\BusinessPlanController@newForm')->name('LAKA business plan.Add business plan.form');
+        Route::get('new', 'BusinessPlan\BusinessPlanController@newForm')->name('LAKA business plan.Add business plan');
         Route::post('new', 'BusinessPlan\BusinessPlanController@new')->name('LAKA business plan.Add business plan');
-        Route::get('update/{id}', 'BusinessPlan\BusinessPlanController@updateForm')->name('LAKA business plan.Update business plan info.form');
+        Route::get('update/{id}', 'BusinessPlan\BusinessPlanController@updateForm')->name('LAKA business plan.Update business plan info');
         Route::post('update/{id}', 'BusinessPlan\BusinessPlanController@update')->name('LAKA business plan.Update business plan info');
         Route::get('delete/{id}', 'BusinessPlan\BusinessPlanController@delete')->name('LAKA business plan.Delete business plan');
     });
@@ -52,12 +52,12 @@ Route::group(['prefix' => 'system-admin', 'middleware' => ['auth', 'permission']
     Route::group(['prefix' => 'user-management'], function () {
         Route::get('/', 'User\UserController@index')->name('LMT user manage.LMT user list');
         Route::get('detail/{id}', 'User\UserController@detail')->name('LMT user manage.LMT user info (detail)');
-        Route::get('update/{id}', 'User\UserController@updateForm')->name('LMT user manage.Update.form[Permission]');
-        Route::post('update/{id}', 'User\UserController@update')->name('LMT user manage.Update.[Permission]');
+        Route::get('update/{id}', 'User\UserController@updateForm')->name('LMT user manage.Update');
+        Route::post('update/{id}', 'User\UserController@update')->name('LMT user manage.Update');
         Route::get('update-password/{id}',
-            'User\UserController@updatePasswordForm')->name('LMT user manage.Update Password.form');
+            'User\UserController@updatePasswordForm')->name('LMT user manage.Update Password');
         Route::post('update-password', 'User\UserController@updatePassword')->name('LMT user manage.Update Password');
-        Route::get('new', 'User\UserController@newForm')->name('LMT user manage.Add LMT user.form');
+        Route::get('new', 'User\UserController@newForm')->name('LMT user manage.Add LMT user');
         Route::post('new', 'User\UserController@register')->name('LMT user manage.Add LMT user');
         Route::get('delete/{id}', 'User\UserController@delete')->name('LMT user manage.LMT user delete');
     });
@@ -66,15 +66,15 @@ Route::group(['prefix' => 'system-admin', 'middleware' => ['auth', 'permission']
     Route::group(['prefix' => 'role'], function () {
         Route::get('/', 'Role\RoleController@index')->name('LMT role manage.Role list');
         Route::get('detail/{id}', 'Role\RoleController@detail')->name('LMT role manage.Detail');
-        Route::get('update/{id}', 'Role\RoleController@updateForm')->name('LMT role manage.Update.form');
+        Route::get('update/{id}', 'Role\RoleController@updateForm')->name('LMT role manage.Update');
         Route::post('update/{id}', 'Role\RoleController@update')->name('LMT role manage.Update');
-        Route::get('new', 'Role\RoleController@newForm')->name('LMT role manage.Add role.form[Permission]');
-        Route::post('new', 'Role\RoleController@register')->name('LMT role manage.Add role.[Permission]');
+        Route::get('new', 'Role\RoleController@newForm')->name('LMT role manage.Add role');
+        Route::post('new', 'Role\RoleController@register')->name('LMT role manage.Add role');
         Route::get('delete/{id}', 'Role\RoleController@delete')->name('LMT role manage.Role delete');
         Route::get('set-permission/{id}',
-            'RoleHasFeatureApi\RoleHasFeatureApiController@setPermissionForm')->name('LMT role manage.Role setting.form[Permission]');
+            'RoleHasFeatureApi\RoleHasFeatureApiController@setPermissionForm')->name('LMT role manage.Role setting');
         Route::post('set-permission/{id}',
-            'RoleHasFeatureApi\RoleHasFeatureApiController@setPermission')->name('LMT role manage.Role setting.[Permission]');
+            'RoleHasFeatureApi\RoleHasFeatureApiController@setPermission')->name('LMT role manage.Role setting');
     });
 
     // version route
@@ -97,7 +97,7 @@ Route::group(['prefix' => 'system-admin', 'middleware' => ['auth', 'permission']
     Route::group(['prefix' => 'user-management-for-app-chat'], function () {
         Route::get('/', 'ApprovalApiToken\ApprovalApiTokenController@index')->name('LAKA user manage.LAKA User list');
         Route::get('detail/{id}', 'UserManagementForAppChat\UserManagementForAppChatController@detailForm')->name('LAKA user manage.Detail');
-        Route::get('new', 'UserManagementForAppChat\UserManagementForAppChatController@newForm')->name('LAKA user manage.Create LAKA user.form');
+        Route::get('new', 'UserManagementForAppChat\UserManagementForAppChatController@newForm')->name('LAKA user manage.Create LAKA user');
         Route::post('new', 'UserManagementForAppChat\UserManagementForAppChatController@register')->name('LAKA user manage.Create LAKA user');
         Route::get('delete/{id}', 'UserManagementForAppChat\UserManagementForAppChatController@delete')->name('LAKA user manage.Delete LAKA user');
     });
