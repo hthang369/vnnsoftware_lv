d77e4b21a1ebb04c028cc6f92b9c6e45f797f3d4
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2021-02-03 11:45:30 +0700

    Seeder for check lost permission

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(RolePermissionSeeder::class);
-         $this->call(ListFunctionSeeder::class);
+        $this->call(ListFunctionSeeder::class);
+        $this->call(RolePermissionSeeder::class);
     }
 }
