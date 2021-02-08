8d15b4464092c752c4551d635f1c4935086428da
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2021-02-08 13:26:00 +0700

    edit clean search

--- resources/views/role/list.blade.php
+++ resources/views/role/list.blade.php
@@ -38,7 +38,7 @@
                 <i class="fa fa-search"></i>
             </button>
             <!-- GET ALL BUTTON -->
-            <a class="ml-3  my-2 btn  btn-secondary" href="/system-admin/user-management" role="button">
+            <a class="ml-3  my-2 btn  btn-secondary" href="/system-admin/user-management/list" role="button">
                 <i class="fa fa-list" aria-hidden="true"></i>
                 @lang('custom_label.get_all')
             </a>
