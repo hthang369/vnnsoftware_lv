2048c01e7bcb123d50e39889e86e4135a1187e77
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2021-02-25 11:38:59 +0700

    update resent

--- resources/views/user-management-for-app-chat/confirm_code.blade.php
+++ resources/views/user-management-for-app-chat/confirm_code.blade.php
@@ -16,8 +16,15 @@
         <form method="get" class="m-5">
             <input type="number" name="code" class="form-control text-center" max="9999" >
             <button type="submit" class="btn btn-danger btn-block mt-4">Submit code for delete user</button>
-            <button type="submit" value="" class="btn btn-warning btn-block mt-4">Resent</button>
+            <button type="submit" value="" class="btn btn-warning btn-block mt-4" id="resent_btn">Resent</button>
         </form>
     @endif
 
+@endsection
+@section('script')
+    <script>
+        $("#resent_btn").click(function(e){
+            $("input[name=code]").val('');
+        })
+    </script>
 @endsection
