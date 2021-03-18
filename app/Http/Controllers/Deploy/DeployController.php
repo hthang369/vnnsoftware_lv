e1211d00627e7a76385a18616912fba8862a791f
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2021-03-18 17:46:06 +0700

    DEPLOY SOURCE

---
+++ app/Http/Controllers/Deploy/DeployController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Deploy;
+
+use App\Http\Controllers\Controller;
+use App\Services\ApprovalApiToken\ApprovalApiTokenService;
+use App\Services\Company\CompanyService;
+use Illuminate\Http\Request;
+
+class DeployController extends Controller {
+
+    public function index() {
+        return view('deploy.list');
+    }
+
+    public function doDeploy(Request $request) {
+        $tag    = $request->input('tag');
+        $server = $request->input('server');
+        $rs =  file_get_contents('http://172.16.3.36:8000/?tag=' . $tag . '&server=' . $server);
+        return redirect(route('Deploy.Deploy index'))->with(['errors'=>$rs]);
+    }
+}
