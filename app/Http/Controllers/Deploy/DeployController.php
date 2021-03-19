83d4eefb1ce8ba57aa2a93de46e9e03e72094ec1
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2021-03-19 17:00:01 +0700

    DEPLOY SOURCE

--- app/Http/Controllers/Deploy/DeployController.php
+++ app/Http/Controllers/Deploy/DeployController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Deploy;
 use App\Http\Controllers\Controller;
 use App\Services\ApprovalApiToken\ApprovalApiTokenService;
 use App\Services\Company\CompanyService;
+use App\Services\DeployService;
 use Illuminate\Http\Request;
 
 class DeployController extends Controller {
@@ -14,15 +15,14 @@ class DeployController extends Controller {
     }
 
     public function doDeploy(Request $request) {
-        $listEnvironment = [
-            'development'=>'http://172.16.2.8:8000',
-            'staging'=>'http://172.16.3.36.:8000',
-            'production'=>'http://laka.lampart-vn.com:8000',
-        ];
+
+        $deployService   = new DeployService();
+        return $deployService->getVersionAPI();
 
         $tag    = $request->input('tag');
         $server = $request->input('server');
-        $rs =  file_get_contents('http://172.16.3.36:8000/?tag=' . $tag . '&server=' . $server);
-        return redirect(route('Deploy.Deploy index'))->with(['errors'=>$rs]);
+        $rs     = file_get_contents('http://172.16.3.36:8000/?tag=' . $tag . '&server=' . $server);
+
+        return redirect(route('Deploy.Deploy index'))->with(['errors' => $rs]);
     }
 }
