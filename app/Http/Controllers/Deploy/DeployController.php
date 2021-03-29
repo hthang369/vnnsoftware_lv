fb0f97e5f100005ad78edd6376c939e040c22c69
dang_khoa <dang_khoa@lampart-vn.com>
2021-03-29 16:31:15 +0700

    update

--- app/Http/Controllers/Deploy/DeployController.php
+++ app/Http/Controllers/Deploy/DeployController.php
@@ -3,21 +3,58 @@
 namespace App\Http\Controllers\Deploy;
 
 use App\Http\Controllers\Controller;
-use App\Services\ApprovalApiToken\ApprovalApiTokenService;
-use App\Services\Company\CompanyService;
-use App\Services\DeployService;
+use App\Models\DeployEnvironment;
 use Illuminate\Http\Request;
+use Laka\Lib\Services\LakaDeploy;
+use function Sodium\add;
 
-class DeployController extends Controller {
-
-    public function index() {
+class DeployController extends Controller
+{
+    public function index()
+    {
         // todo: gọi api để lấy dữ liệu về
-        return view('deploy.list');
+
+        $environmentArray = [];
+
+        foreach (config('deploy.list_environment') as $environment => $type) {
+            // array of DeployEnvironment objects
+            $deployEnvironmentArray = [];
+            foreach ($type as $typeName => $value) {
+                $deployEnvironment = new DeployEnvironment();
+                $deployEnvironment->set_environment($environment);
+                $deployEnvironment->set_type($typeName);
+                $deployEnvironment->set_version(DeployEnvironment::getVersion($environment, $typeName));
+                // add DeployEnvironment object to array
+                $deployEnvironmentArray[] = $deployEnvironment;
+            }
+            // add array of DeployEnvironment objects to 1 environment
+            $environmentArray[$environment] = $deployEnvironmentArray;
+        }
+        return view('deploy.list')->with(['environmentArray' => $environmentArray]);
     }
 
-    public function doDeploy(Request $request) {
+    public function doDeploy(Request $request)
+    {
         // todo: gọi api lên server để deploy
+        $result = LakaDeploy::deploy(
+            $request->get('environment'),
+            $request->get('type'),
+            $request->get('version')
+        );
 
-        return redirect(route('Deploy.Deploy index'))->with(['errors' => []]);
+        $status = $result['development']['status'];
+        $environment = $request->get('environment');
+        $type = $request->get('type');
+        $version = $request->get('version');
+        $message = $result['development']['data']->return[0];
+
+        return redirect(route('Deploy.Deploy index'))
+            ->with([
+                'status' => $status,
+                'environment' => $environment,
+                'type' => $type,
+                'version' => $version,
+                'message' => $message,
+            ]);
     }
 }
--- resources/views/deploy/list.blade.php
+++ resources/views/deploy/list.blade.php
@@ -7,80 +7,43 @@
 @endsection
 
 @section('content')
-    <h2>Server Frontend</h2>
-    <div class="well">
-        <h3>Environment: <b>Dev</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-        @csrf
-        <input class="form-control" name="tag" placeholder="ID commit / Version">
-        <input type="hidden" class="form-control" name="server" value="frontend">
-        <button class="btn btn-primary">
-            Deploy
-        </button>
-    </form>
-    </div>
-    <div class="well">
-        <h3>Environment: <b>Staging</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-            @csrf
-            <input class="form-control" name="tag" placeholder="ID commit / Version">
-            <input type="hidden" class="form-control" name="server" value="frontend">
-            <button class="btn btn-primary">
-                Deploy
-            </button>
-        </form>
-    </div>
-    <div class="well">
-        <h3>Environment: <b>Production</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-            @csrf
-            <input class="form-control" name="tag" placeholder="ID commit / Version">
-            <input type="hidden" class="form-control" name="server" value="frontend">
-            <button class="btn btn-primary">
-                Deploy
-            </button>
-        </form>
-    </div>
-    <hr>
-    <h2>Server Backend</h2>
-    <div class="well">
-        <h3>Environment: <b>Dev</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-            @csrf
-            <input class="form-control" name="tag" placeholder="ID commit / Version">
-            <input type="hidden" class="form-control" name="server" value="frontend">
-            <button class="btn btn-primary">
-                Deploy
-            </button>
-        </form>
-    </div>
-    <div class="well">
-        <h3>Environment: <b>Staging</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-            @csrf
-            <input class="form-control" name="tag" placeholder="ID commit / Version">
-            <input type="hidden" class="form-control" name="server" value="frontend">
-            <button class="btn btn-primary">
-                Deploy
-            </button>
-        </form>
-    </div>
-    <div class="well">
-        <h3>Environment: <b>Production</b></h3>
-        <div>Current version: <b>v1.1</b></div>
-        <form method="post">
-            @csrf
-            <input class="form-control" name="tag" placeholder="ID commit / Version">
-            <input type="hidden" class="form-control" name="server" value="frontend">
-            <button class="btn btn-primary">
-                Deploy
-            </button>
-        </form>
-    </div>
+    @if(session()->has('status'))
+        <div class="alert {{session()->get('status') == true ? 'alert-success' : 'alert-danger' }}">
+            Deployed to:  <strong>server {{session()->get('environment')}}</strong>
+            <br>
+            Environment: <strong>{{session()->get('type')}}</strong>
+            <br>
+            Input version: <strong>{{session()->get('version')}}</strong>
+            <br>
+            Message: <strong> {{session()->get('message')}}</strong>
+            <br>
+        </div>
+    @endif
+    @foreach($environmentArray as $env => $obj)
+        <div class="card mb-5">
+            <div class="text-white card-header bg-primary">
+                <h2>Server {{$env}}</h2>
+            </div>
+            @foreach($obj as $key => $value)
+                <div class="card-body ">
+                    <div class="well mb-1">
+                        <h3>Environment: <b>{{$value->type}}</b></h3>
+                        <div>Current version: <b>{{$value->version}}</b></div>
+                        <form method="post">
+                            @csrf
+                            <input class="form-control" name="version" placeholder="ID commit / Version">
+                            <input type="hidden" class="form-control" name="environment" value="{{$env}}">
+                            <input type="hidden" class="form-control" name="type" value="{{$value->type}}">
+                            <button class="btn btn-primary mt-1">
+                                Deploy
+                            </button>
+                        </form>
+                    </div>
+
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+
 @endsection
 
