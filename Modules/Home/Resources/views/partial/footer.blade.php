6d9ae636362cd675e502c30bbaecedd2c619f846
[Lampart] Tran Hoang Thang <hoang_thang@lampart-vn.com>
2021-09-05 16:55:20 +0700

    update

--- Modules/Home/Resources/views/partial/footer.blade.php
+++ Modules/Home/Resources/views/partial/footer.blade.php
@@ -43,11 +43,7 @@
         © Copyright <strong><span>VNNIT</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mybiz-free-business-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="http://webdesignvnnit.com/">VNN Computer</a>
       </div>
     </div>
   </footer>
