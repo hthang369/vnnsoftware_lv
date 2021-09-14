69733cefd40446356d7e2f2c760ab48811f6de08
dangkhoa <dangkhoa.lampart@gmail.com>
2021-09-14 10:29:02 +0700

    add download laka log migrate

---
+++ database/migrations/2021_09_14_032450_create_download_laka_log_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDownloadLakaLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('download_laka_log', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('download_laka_log');
+    }
+}
